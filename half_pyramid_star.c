18c4cc9bf3d3e2b51142343f0692ab53a4b6c642
OmNikam04 <omnikam0404@gmail.com>
2021-03-15 21:19:04 +0530

    Patterns_type_A

--- half_pyramid_star.c
+++ half_pyramid_star.c
@@ -8,11 +8,11 @@
 
 int main()
 {
-    int row , i, j;
-    scanf("%d",&row);
-    for ( i = 0; i <= row; i++)
+    int n , i, j;
+    scanf("%d",&n);
+    for ( i = 1; i <= n; i++)
     {
-        for ( j = 0; j <= i; j++)
+        for ( j = 1; j <= i; j++)
         {
             printf("*\t");
         }
